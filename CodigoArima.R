133114a6ac09e9a9e111d11903b96cc03e7a7dfb
Vegely <79764272+Vegely@users.noreply.github.com>
2026-01-28 18:45:21 +0100

    Add time series decomposition and plotting to ARIMA script
    Expanded CodigoArima.R to include time series creation, plotting, and decomposition (both additive and multiplicative) using the 'timsac' library. The script now visualizes the original and seasonally adjusted series. Also added a new .Rhistory file capturing R command history.

--- CodigoArima.R
+++ CodigoArima.R
@@ -1,2 +1,28 @@
+# Carga de datos y análisis preliminar de los mismos
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 data <- read.csv("data_monthly.csv")
+data.ts <-ts(data$MONTHLY_AVG, start = c(2010,1), frequency = 12)
+data.ts
+plot(data.ts)
+
+# Tema 1: Tratamiento mediante suavizado de los datos
+library(timsac)
+
+# Por defecto de tipo "aditivo"
+descomp1=decompose(data.ts)
+plot(descomp1)
+data.tsDES=data.ts-descomp1$seasona
+plot(data.tsDES,col="blue")
+par(new=T)
+plot(data.ts,col="red")
+
+# Cambiamos a tipo "multiplicativo"
+descomp1=decompose(data.ts,type="multiplicative")
+plot(descomp1)
+data.tsDES=data.ts-descomp1$seasona
+plot(data.tsDES,col="blue")
+par(new=T)
+plot(data.ts,col="red")
+
+
+# Tema 2: 
